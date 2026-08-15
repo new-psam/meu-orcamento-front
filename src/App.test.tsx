005328dac5f7df1a22676e028e31ea7ac046522a
new-psam <marcelino.patricio@hotmail.com>
2026-08-15 15:53:12 -0300

    chore: setup inicial do CI, vitest e lint

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect }  from 'vitest';
+
+describe('Configuração inicial', ()=> {
+    it('deve passar no teste básico', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
