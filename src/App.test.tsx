d3a0c1c0dd733e5aa3d075572331550619cc8a51
new-psam <marcelino.patricio@hotmail.com>
2026-08-16 08:41:15 -0300

    chorus: ajuste em teste teste

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,6 @@ import { describe, it, expect }  from 'vitest';
 
 describe('Configuração inicial', ()=> {
     it('deve passar no teste básico', () => {
-        expect(1 + 1).toBe(2);
+        expect(2 + 1).toBe(3);
     });
 });
